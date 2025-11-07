2afc9a2c743e8a10932d296d7fedb366ed27cea3
igor0101-gharo <igor.v.ferreira@ba.estudante.senai.br>
2025-11-07 18:06:38 +0000

    aulas

--- Aulas_agentes.py
+++ Aulas_agentes.py
@@ -13,6 +13,7 @@ executar = st.button("Gerar material")
 api_key = 'SUA_CHAVE_API'
 
 if executar:
+    #características
     llm = LLM(
         model="groq/llama-3.3-70b-versatile",
         api_key=api_key,
@@ -22,3 +23,43 @@ if executar:
         #entre 0.4 e 0.7 equilibrado para explicação
         #maior que 0.7 mais criativo
     )
+
+#Agentes
+agente_resumo = agent(
+    role="Redator de resumo didático",
+    # O que deve fazer. 
+    goal=(
+        "Escrever RESUMO claro e didático sobre {tema} alinhado com o {objetivo}."
+        "A linguagem deve ser didática, direta com contexto prático e sem jargões"
+    ),
+    backstory= "Você transforma temas técnicos/acadêmicos em explicações curtas e precisas.",
+    llm= llm, verbose=False
+)
+
+agente_exemplos = agent(
+    role="Criador de exemplos contextualizados",
+    goal=(
+        "Gerar 5 EXEMPLOS CURTOS sobre {tema}, cada um com contexto realista."
+        "Cada exemplo com título (em negrito), cenário, dados(se houver), aplicação e resultado"
+    ),
+    backstory= "Você mostra o conceito em ação com exemplos breves e concretos.",
+    llm=llm, verbose=false
+)
+
+agente_exercicios = agent(
+    role="Criador de exercícios",
+    goal=(
+        "Criar 4 EXERCÍCIOS SIMPLES sobre {tema}"
+        "Variar formato (múltipla escolha, V/F, completar, resolução curta)."
+        "Enunciados claros. NÃO incluir respostas"
+    ),
+    backstory= "Você cria atividades rápidas qie fixam conceitos essenciais",
+    llm=llm, verbose=false
+)
+agente_gabarito = agent(
+    role="Revisor e gabaritador",
+    goal=(
+        "Ler os EXERCÍCIOS sobre {tema} e produzir GABARITO oficial, com respostas corretas e justificativa breve (1-3 frases) por item"
+    ),
+    backstory= "Você confere consistência e explica rapidamente o porquê da resposta.",
+    llm=llm, verbose=false
